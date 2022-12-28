948478d0adde4e42404d05fcd28bc79d48244bc5
Prashmin Gurung <prashmingrg@gmail.com>
2022-12-28 20:40:04 +1100

    some changes

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import Path from "path";
+import path from "path";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /******* CONFIGURATION  *******/
+
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.__dirname(__filename);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
@@ -20,8 +24,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodParser.json({ limit: "30mb", extended: true }));
-app.use(bodParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assests", express.static(path.join(__dirname, "public/assets")));
 
@@ -31,20 +35,32 @@ const storage = multer.diskStorage({
     cb(null, "public/assets");
   },
   filename: function (req, file, cb) {
-    cb(null, full.originalname);
+    cb(null, full.originalname); //Cb = Call Back
   },
 });
 const upload = multer({ storage });
 
+/******* ROUTES WITH FILES AUTH  */
+app.post("/auth/register", upload.single("picture"), register);
+
+/*** ROUTES */
+app.use("auth", authRoutes);
+app.use("/users", userRoutes);
+
 /******* MONGOOSE SETUP *********/
 
 const PORT = process.env.PORT || 6001;
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log("server PORT: ${PORT}"));
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
-  .catch((error) => console.log("${error} did not connect"));
+  .catch((error) => console.log(`${error} did not connect`));
